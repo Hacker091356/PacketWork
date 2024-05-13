c3540eccc9def795a0da426ac959d2ad8a9b179d
Hacker091356 <ninno2812@gmail.com>
2024-05-13 14:59:54 -0400

    Update BoxRunner.java

--- Box/BoxRunner.java
+++ Box/BoxRunner.java
@@ -1,6 +1,15 @@
 public class BoxRunner {
     public static void main(String[] args){
         Box bob = new Box(3);
-        int v 
+        int v = bob.getVolume();
+        System.out.println( v + "cu.in");
+
+        Box ann = new Box(4);
+        v = (ann.getVolume9);
+        System.put.print (v + "cu.in");
+        
+        ann.change(2);
+        v = ann.getVolume();
+        System.out.println(V + "cu.in");
     }
 }
