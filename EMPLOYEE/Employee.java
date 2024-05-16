d2ae768c3aa4375dcdf29248185b86f83e33e5be
Hacker091356 <ninno2812@gmail.com>
2024-05-16 14:44:18 -0400

    0

--- EMPLOYEE/Employee.java
+++ EMPLOYEE/Employee.java
@@ -1,3 +1,17 @@
-public class Employee {
+public class Employee{
+    double salary = 0;
+    public Employee(double s){
+        salary = s;
+
     
+    }
+    public void increase(double i){
+        if (i > 0){
+            salary = salary + i;
+        }
+    
+    }
+    public double getPay(){
+        return salary;
+    }
 }
--- EMPLOYEE/EmployeeRunner.java
+++ EMPLOYEE/EmployeeRunner.java
@@ -1,3 +1,16 @@
 public class EmployeeRunner {
-    
+    public static void main(String[] args) {
+        Employee Neil = new Employee(40000.0);
+        Neil.increase(2000.0);
+        double p = Neil.getPay();
+        System.out.println("Salary is " + p);
+
+        Neil.increase(-500.0);
+        p = Neil.getPay();
+        System.out.print("Salary is " + p);
+
+        Neil.increase(3000.0);
+        p = Neil.getPay();
+        System.out.println("Salary is " + p);
+    }
 }
